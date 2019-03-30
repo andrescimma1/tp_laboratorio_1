2a629989e4e63ea58b12bb270196ac4d5b5a52c6
Andy <andydile@gmail.com>
2019-03-29 22:51:00 -0300

    tp terminado sin errores ni warnings

--- tp_laboratorio_1/main.c
+++ tp_laboratorio_1/main.c
@@ -4,14 +4,14 @@
 
 int main()
 {
-    int x, y, opcion;
+    int x = 0, y = 0, opcion;
 
     do{
 
         system("cls");
         printf("\t\t*** MENU DE OPCIONES ***");
-        printf("\n\n 1. Ingresar 1er operando: ");
-        printf("\n 2. Ingresar 2do operando: ");
+        printf("\n\n 1. Ingresar 1er operando: (A=%d)" , x);
+        printf("\n 2. Ingresar 2do operando: (B=%d)", y);
         printf("\n 3. Calcular todas las operaciones");
         printf("\n\ta) Calcular la suma (A+B)");
         printf("\n\tb) Calcular la resta (A-B)");
@@ -34,11 +34,16 @@ int main()
             system("cls");
             printf("Ingrese el 2do operando: ");
             scanf("%d", &y);
+            while(y == 0)
+            {
+                printf("No se puede dividir por 0!! Ingrese el 2do operando: ");
+                scanf("%d", &y);
+            }
             break;
         case 3:
             system("cls");
-            printf("Todas las operaciones fueron calculadas... \n\n Por favor, en el menu de opciones digite la opcion 4");
-            getch();
+            printf("Todas las operaciones fueron calculadas... \n\n Por favor, en el menu de opciones digite la opcion 4\n\n");
+            system("pause");
             break;
         case 4:
             system("cls");
@@ -47,8 +52,8 @@ int main()
             printf("\na) El resultado de %d - %d es: %d", x, y, Restar(x,y));
             printf("\nb) El resultado de %d / %d es: %d", x, y, Dividir(x,y));
             printf("\nc) El resultado de %d * %d es: %d", x, y, Multiplicar(x,y));
-            printf("\nd) El factorial de %d es: %d y El factorial de %d es: %d", x, Factorial1(x), y, Factorial2(y));
-            getch();
+            printf("\nd) El factorial de %d es: %d y El factorial de %d es: %d\n\n", x, Factorial1(x), y, Factorial2(y));
+            system("pause");
             break;
         case 5:
             break;
